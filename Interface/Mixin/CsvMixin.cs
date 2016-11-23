9e7e255c2e503e5286f2e0629b0f6d8cc1c4a389
soudog <soudog@outlook.com>
2016-11-23 09:58:14 +0800

    f

---
+++ Interface/Mixin/CsvMixin.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using Trade.Mixin;
+
+namespace Interace.Mixin
+{
+    public static class CsvMixin
+    {
+        public static IEnumerable<T> ReadCsv<T>(this string path) where T:new()
+        {
+            if (!File.Exists(path))
+                return Enumerable.Empty<T>();
+
+            var lines = File.ReadAllLines(path);
+            if (!lines.Any())
+                return Enumerable.Empty<T>();
+
+            var columns = lines[0].Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            return lines
+                .Skip(1)
+                .Select(p =>
+                {
+                    var splits = p.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                    var f = new T();
+                    for (var i = 0; i < columns.Length; ++i)
+                    {
+                        var column = columns[i];
+                        f.SetPropertyValue(column, splits[i]);
+                    }
+                    return f;
+                })
+                .ToArray();
+        }
+    }
+}
---
+++ Interface/Mixin/PathMixin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Interace.Mixin
+{
+    public static class PathMixin
+    {
+        public static string EnsurePathCreated(this string path)
+        {
+            var s = new Stack<string>();
+            var dir = path;
+            while (true)
+            {
+                if (string.IsNullOrEmpty(dir) || Directory.Exists(dir))
+                    break;
+
+                s.Push(dir);
+
+                dir = Path.GetDirectoryName(path);
+            }
+
+            while (s.Count > 0)
+            {
+                Directory.CreateDirectory(s.Pop());
+            }
+
+            return path;
+        }
+    }
+}
