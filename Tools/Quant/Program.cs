48a41fccb66bc07afd5d3d303340f87a945eaec2
soudog <soudog@outlook.com>
2016-11-29 08:11:32 +0800

    f

--- Tools/Quant/Program.cs
+++ Tools/Quant/Program.cs
@@ -22,12 +22,12 @@ namespace Quant
             log.Warn("***********************START***********************");
 
             log.Info("run strategy");
-            //var s = new BackMA120Strategy();
-            //s.Run(StrategyIn.Default);
+            var s = new BackMA120Strategy();
+            s.Run(StrategyIn.Default);
             //var s = new LowBetaStrategy(junxianduotou:false, beta:0.5);
             //s.Run(StrategyIn.Default);
-            var s = new LowPEStrategy();
-            s.Run(StrategyIn.Default);
+            //var s = new LowPEStrategy();
+            //s.Run(StrategyIn.Default);
 
             log.Warn("***********************DONE***********************");
         }
