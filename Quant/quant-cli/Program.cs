f0a0252c34d83829c0f134a9a80792105af3d1a8
soudog <soudog@outlook.com>
2016-11-29 22:15:28 +0800

    s

--- Quant/quant-cli/Program.cs
+++ Quant/quant-cli/Program.cs
@@ -45,7 +45,7 @@ namespace Quant
                         var codes = client.Codes(parameters.sector ?? string.Empty);
 
                         log.Info("run selection");
-                        var pool = new Trade.Selections.Impl.LowBeta().Pass(codes);
+                        var pool = new Trade.Selections.Impl.LowBeta(junxianduotou:false).Pass(codes);
                         log.WarnFormat("{0} selections", pool.Count);
 
                         var quant = new LowBeta();
@@ -57,7 +57,7 @@ namespace Quant
                         log.WarnFormat("{0} trades", account.Trades.Count);
                         if (account.Trades.Any())
                         {
-                            log.Info("save down selections");
+                            log.Info("save down trades");
                             var path = Configuration.oms.trade.EnsurePathCreated();
 
                             File.WriteAllText(Path.Combine(path, DateTime.Today.ToString("yyyy-MM-dd")+".csv"), account.Trades.ToCsv(), Encoding.UTF8);
