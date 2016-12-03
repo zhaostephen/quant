3ec073868f4fa3c8137f44e2deb9fe9592d07fd8
soudog <soudog@outlook.com>
2016-12-03 16:31:43 +0800

    f

---
+++ Server/server-cli/commands/updateconfig.cs
@@ -0,0 +1,50 @@
+﻿using Cli;
+using CommandLine;
+using log4net;
+using ServiceStack;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using Trade.Data;
+
+namespace Trade.Cli.commands
+{
+    [command("updateconfig")]
+    class updateconfigcmd : command<updateconfigcmd.Parameters>
+    {
+        static ILog log = LogManager.GetLogger(typeof(updateconfigcmd));
+
+        public updateconfigcmd(string[] args) : base(args) { }
+
+        public override void exec()
+        {
+            foreach(var file in Directory.GetFiles(param.path,"*.config"))
+            {
+                log.InfoFormat("update " + file);
+
+                var e = XElement.Load(file);
+                var q = e.XPathSelectElement("//appSettings/add[@key='quant']");
+                if(q==null)
+                {
+                    log.WarnFormat("ignore " + file);
+                    continue;
+                }
+                q.SetAttributeValue("value", param.quant);
+                e.Save(file);
+            }            
+        }
+
+        public class Parameters
+        {
+            [Option('p', "path", Required = true)]
+            public string path { get; set; }
+            [Option('q', "quant", Required = true)]
+            public string quant { get; set; }
+        }
+    }
+}
---
+++ deploy.bat
@@ -0,0 +1,2 @@
+robocopy bin \\139.199.73.92\c$\quant\bin /PURGE
+\\139.199.73.92\c$\quant\bin\server-cli updateconfig --p \\139.199.73.92\c$\quant\bin --q C:\quant\data
