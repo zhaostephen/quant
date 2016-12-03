006f8116e90fcd5627fc34624de25ab47f67e781
soudog <soudog@outlook.com>
2016-12-03 17:09:35 +0800

    f

--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,7 @@
+@echo off
+echo **********************copy data**********************
 robocopy data/raw/fundamental \\139.199.73.92\c$\quant\data\raw\fundamental /PURGE
+echo **********************deploy server**********************
 robocopy bin \\139.199.73.92\c$\quant\bin /PURGE
+echo **********************update server config**********************
 \\139.199.73.92\c$\quant\bin\server-cli updateconfig --p \\139.199.73.92\c$\quant\bin --q C:\quant\data
