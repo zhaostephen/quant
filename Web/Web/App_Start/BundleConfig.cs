99744bf4ab300c21f54858012962de187f5fdabc
soudog <soudog@outlook.com>
2016-12-03 15:46:27 +0800

    f

--- Web/Web/App_Start/BundleConfig.cs
+++ Web/Web/App_Start/BundleConfig.cs
@@ -10,9 +10,10 @@ namespace Web
         {
             bundles.Add(new ScriptBundle("~/bundles/app")
                 .Include("~/app/chart.js")
-                .IncludeDirectory("~/app/services", "*.js", true)
-                .IncludeDirectory("~/app/directives", "*.js", true)
-                .IncludeDirectory("~/app/controllers", "*.js", true));
+                //.IncludeDirectory("~/app/services", "*.js", true)
+                //.IncludeDirectory("~/app/directives", "*.js", true)
+                //.IncludeDirectory("~/app/controllers", "*.js", true)
+                );
 
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
