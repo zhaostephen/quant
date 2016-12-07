34fd449ad4a389d026c60986e3c209e6dc5d5b88
soudog <soudog@outlook.com>
2016-12-07 20:20:22 +0800

    f

--- Server/Cluster/run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo ***********************fundamentals***********************
-server fundamental
-echo ***********************index***********************
-index
-echo ***********************mktdata***********************
-cluster
-echo ***********************keyprice***********************
-server keyprice
