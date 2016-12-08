c6882dc596307a536f126737bd538e06138b089e
soudog <soudog@outlook.com>
2016-12-08 20:43:03 +0800

    df

--- Interface/Attribution/DEVIATION.cs
+++ Interface/Attribution/DEVIATION.cs
@@ -13,24 +13,20 @@ namespace Interace.Attribution
             var cross_up = cross(k, (i, next) => i.macd < 0 && next.macd >= 0);
             var cross_down = cross(k, (i, next) => i.macd >= 0 && next.macd < 0);
 
-            for (var i = 0; i < cross_up.Count - 1; ++i)
+            for (var i = 1; i < cross_up.Count; ++i)
             {
-                var c1 = cross_up[i + 1].macd - cross_up[i].macd;
-                var c2 = cross_up[i + 1].close - cross_up[i].close;
+                var c1 = cross_up[i].macd - cross_up[i-1].macd;
+                var c2 = cross_up[i].close - cross_up[i-1].close;
                 var deviated = c1 >= 0 && c2 <= 0;
                 if (!deviated)
                     continue;
 
-                //var k1 = (cross_up[i + 1].close - cross_up[i].close) / dates;
-                //var k2 = (cross_up[i + 1].dif.Value - cross_up[i].dif.Value) / dates;
-                //var angle = Math.Atan(Math.Abs(k2 - k1) / (1 + k1 * k2));
-                var units = k.Count(p => p.date >= cross_up[i].date && p.date <= cross_up[i + 1].date);
+                var units = k.Count(p => p.date >= cross_up[i-1].date && p.date <= cross_up[i].date);
                 var deviation = new deviation
                 {
-                    d1 = cross_up[i].date,
-                    d2 = cross_up[i + 1].date,
-                    cross = units,
-                    //angle = angle
+                    d1 = cross_up[i-1].date,
+                    d2 = cross_up[i].date,
+                    cross = units
                 };
 
                 if (deviation.cross > tolerance)
@@ -41,15 +37,15 @@ namespace Interace.Attribution
         static kdata cross(kdata k, Func<kdatapoint, kdatapoint, bool> cmp)
         {
             var cross = new List<kdatapoint>();
-            for (var i = 0; i < k.Count - 1; ++i)
+            for (var i = 1; i < k.Count; ++i)
             {
-                if (cmp(k[i], k[i + 1]))
+                if (cmp(k[i-1], k[i]))
                 {
-                    if (Math.Abs(k[i].dea.Value - k[i].dif.Value) <
-                        Math.Abs(k[i + 1].dea.Value - k[i + 1].dif.Value))
-                        cross.Add(k[i]);
+                    if (Math.Abs(k[i-1].dea.Value - k[i-1].dif.Value) <
+                        Math.Abs(k[i].dea.Value - k[i].dif.Value))
+                        cross.Add(k[i-1]);
                     else
-                        cross.Add(k[i + 1]);
+                        cross.Add(k[i]);
                 }
             }
             return new kdata(k.Code, cross);
