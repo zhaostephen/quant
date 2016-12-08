f69ed3543390cf10dba2d087ecd92f0fc75cca6c
soudog <soudog@outlook.com>
2016-12-08 23:40:18 +0800

    f

--- quant/flex/sms.cs
+++ quant/flex/sms.cs
@@ -5,12 +5,79 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
 
 namespace Quant.flex
 {
+    //public class sms2
+    //{
+    //    private const String host = "http://sms.market.alicloudapi.com";
+    //    private const String path = "/singleSendSms";
+    //    private const String method = "GET";
+    //    private const String appcode = "f665f176e7194eb181e28d2bff799a72";
+
+    //    public void order(Interace.Quant.Trade trade)
+    //    {
+    //        var message = HttpUtility.UrlEncode(string.Format("{0:yyyy-MM-dd HH:mm:ss} | {1} {2}({3})", trade.Date, trade.Dir, trade.Stock, trade.Portflio));
+    //        String querys = "ParamString=" +"&RecNum=15801427695";
+    //        String bodys = "";
+    //        String url = host + path;
+    //        HttpWebRequest httpRequest = null;
+    //        HttpWebResponse httpResponse = null;
+
+    //        if (0 < querys.Length)
+    //        {
+    //            url = url + "?" + querys;
+    //        }
+
+    //        if (host.Contains("https://"))
+    //        {
+    //            ServicePointManager.ServerCertificateValidationCallback = new RemoteCertificateValidationCallback(CheckValidationResult);
+    //            httpRequest = (HttpWebRequest)WebRequest.CreateDefault(new Uri(url));
+    //        }
+    //        else
+    //        {
+    //            httpRequest = (HttpWebRequest)WebRequest.Create(url);
+    //        }
+    //        httpRequest.Method = method;
+    //        httpRequest.Headers.Add("Authorization", "APPCODE " + appcode);
+    //        if (0 < bodys.Length)
+    //        {
+    //            byte[] data = Encoding.UTF8.GetBytes(bodys);
+    //            using (Stream stream = httpRequest.GetRequestStream())
+    //            {
+    //                stream.Write(data, 0, data.Length);
+    //            }
+    //        }
+    //        try
+    //        {
+    //            httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+    //        }
+    //        catch (WebException ex)
+    //        {
+    //            httpResponse = (HttpWebResponse)ex.Response;
+    //        }
+
+    //        Console.WriteLine(httpResponse.StatusCode);
+    //        Console.WriteLine(httpResponse.Method);
+    //        Console.WriteLine(httpResponse.Headers);
+    //        Stream st = httpResponse.GetResponseStream();
+    //        StreamReader reader = new StreamReader(st, Encoding.GetEncoding("utf-8"));
+    //        Console.WriteLine(reader.ReadToEnd());
+    //        Console.WriteLine("\n");
+
+    //    }
+
+    //    public static bool CheckValidationResult(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors errors)
+    //    {
+    //        return true;
+    //    }
+
+    //}
     public class sms
     {
         static ILog log = typeof(sms).Log();
