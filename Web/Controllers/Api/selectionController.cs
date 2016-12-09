1ef3b07b2f92ab05dd09e99411c5deaed4a31cba
soudog <soudog@outlook.com>
2016-12-09 23:42:14 +0800

    d

---
+++ Web/Controllers/Api/selectionController.cs
@@ -0,0 +1,27 @@
+﻿using ServiceStack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Trade;
+using Interace.Attribution;
+using Trade.Cfg;
+using Trade.Data;
+
+namespace Web.Controllers.Api
+{
+    public class selectionController : ApiController
+    {
+        [Route("api/selections")]
+        public object[] Get()
+        {
+            var codenames = new client().basicnames().GroupBy(p=>p.code).ToDictionary(p => p.Key, p => p.First().name);
+            var universe = new client().universe("macd60");
+            return universe.codes
+                .Select(p => new { universe.name, code = p, stockname = codenames[p] })
+                .ToArray();
+        }
+    }
+}
---
+++ Web/app/controllers/selectionCtrl.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    'use strict';
+
+    var ctrl = ['$scope', '$http', '$interval', function ($scope, $http, $interval) {
+        $scope.universe = [];
+
+        $scope.refresh = function () {
+            $http.get(root + 'api/selections')
+               .then(function (res) {
+                   $scope.universe = res.data;
+               }, function (res) { });
+        }
+
+        $scope.refresh();
+    }];
+
+    angular.module('app').controller('selectionCtrl', ctrl)
+})();
