3d598b23df1af4ef28f7f92fbacec38959f315b1
soudog <soudog@outlook.com>
2016-12-10 22:33:29 +0800

    d

---
+++ quant/commands/strategycmd.cs
@@ -0,0 +1,56 @@
+﻿using Cli;
+using CommandLine;
+using Interace.Quant;
+using Interface.Quant;
+using log4net;
+using System.Linq;
+using Trade;
+
+namespace Quant.commands
+{
+    [command("strategy")]
+    class strategycmd : command<strategycmd.parameters>
+    {
+        static ILog log = LogManager.GetLogger(typeof(strategycmd));
+
+        public strategycmd(string[] args) : base(args) { }
+
+        public override void exec()
+        {
+            log.Info("run strategy " + param.name.ToLower());
+
+            log.Info("get codes");
+            var universe = param.asector 
+                ? new universe(param.universe, new client().codes(param.universe).ToArray())
+                : getUniverse(param.universe);
+            log.Info("total " + universe.codes.Length);
+
+            log.Info("get run");
+            var pool = new StockPool(universe.codes);
+            var account = new Account(param.name.ToLower(), pool);
+            switch (param.name.ToLower())
+            {
+                case "kdj15min":
+                    new strategies.KDJ15minStrategy().Run(account);
+                    break;
+            }
+
+            log.Info("**********DONE**********");
+        }
+
+        private universe getUniverse(string name)
+        {
+            return new client().universe(name);
+        }
+
+        public class parameters
+        {
+            [Option('n', "name", Required = true)]
+            public string name { get; set; }
+            [Option('u', "universe", DefaultValue = "default", Required = false)]
+            public string universe { get; set; }
+            [Option('a', "asector", DefaultValue = false, Required = false)]
+            public bool asector { get; set; }
+        }
+    }
+}
