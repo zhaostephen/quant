6e76e1779aa3f1d2cacf4174d6cd778f53ca0ced
soudog <soudog@outlook.com>
2016-12-21 20:58:03 +0800

    f

--- Web/Controllers/Api/kdataController.cs
+++ Web/Controllers/Api/kdataController.cs
@@ -29,13 +29,20 @@ namespace Web.Controllers.Api
             var keyprices = q.Select(p =>
             {
                 var h = peak_h[p.date];
-                var l = peak_h[p.date];
+                var l = peak_l[p.date];
                 if (h > 0) return KeyPrice.high(id, p.date, h, true);
                 else if (l > 0) return KeyPrice.low(id, p.date, l, true);
                 return null;
             })
             .Where(p => p != null)
             .ToArray();
+
+            var cur = new[] 
+            {
+                KeyPrice.low(id, q.Last().date, q.Last().low, true),
+                KeyPrice.high(id, q.Last().date, q.Last().high, true)
+            };
+            keyprices = keyprices.Concat(cur).ToArray();
 
             return new chart { data = data, code = d.Code, name = basic.name, keyprices = keyprices };
         }
