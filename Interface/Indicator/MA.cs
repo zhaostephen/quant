f127877a6f3e63dbd6063b5565060b6f9e753bdd
soudog <soudog@outlook.com>
2016-12-30 22:59:35 +0800

    f

--- Interface/Indicator/MA.cs
+++ Interface/Indicator/MA.cs
@@ -9,28 +9,19 @@ namespace Trade.Indicator
 {
     public class MA : Series<double>
     {
-        public MA(Series<double> data, int N)
+        public MA(Series<double> a, int N)
         {
-            for (int i = N - 1; i < data.Count; i++)
+            for (var i = 0; i < a.Count; i++)
             {
-                var point = data[i];
-                var section = data.Skip((i + 1) - N).Take(N);
-                if (section.Count() < N)
-                    break;
-
-                var value = Math.Round(section.Select(p => p.Value).Average(), 2);
-                if (value == 0d)
-                    continue;
-
-                Add(new sValue<double>
-                {
-                    Date = point.Date,
-                    Value = value,
-                });
+                var sum = 0d;
+                var j = 0;
+                for (j = 0; j < N && i - j >= 0; ++j)
+                    sum += a[i - j].Value;
+                Add(a[i].Date, Math.Round(sum / j, 2));
             }
         }
 
-        public static implicit operator double?(MA o)
+        public static implicit operator double? (MA o)
         {
             return o.Any() ? o.Last().Value : default(double?);
         }
