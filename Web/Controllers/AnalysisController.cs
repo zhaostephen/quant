9e830b72b7f71df13f649ec8fded0cad89585b87
soudog <soudog@outlook.com>
2017-01-12 08:50:54 +0800

    sdf

--- Web/Controllers/AnalysisController.cs
+++ Web/Controllers/AnalysisController.cs
@@ -24,8 +24,9 @@ namespace Web.Controllers
             return View();
         }
 
-        public ActionResult singlename()
+        public ActionResult singlename(string sector)
         {
+            ViewBag.sector = sector;
             return View();
         }
     }
