0c0d1a89307b2071476732e649f15ed293b916d9
soudog <soudog@outlook.com>
2019-07-09 21:23:13 +0800

    add deploy-remote.bat

--- deploy.bat
+++ deploy.bat
@@ -1,8 +1,8 @@
 @echo off
 echo **********************copy data**********************
-robocopy data/raw/basics \\139.199.73.92\c$\quant\data\raw\basics *.xls /PURGE
+robocopy data/raw/basics c:\quant\data\raw\basics *.xls /PURGE
 echo **********************deploy server**********************
-robocopy bin \\139.199.73.92\c$\quant\bin /PURGE
+robocopy bin c:\quant\bin /PURGE
 echo **********************update server config**********************
-\\139.199.73.92\c$\quant\bin\deploy updateconfig --p \\139.199.73.92\c$\quant\bin --q C:\quant\data --n 0-1800,1800-3600 --t C:/quant/data/
+c:\quant\bin\deploy updateconfig --p c:\quant\bin --q C:\quant\data --n 0-1800,1800-3600 --t C:/quant/data/
 echo **********************done**********************
